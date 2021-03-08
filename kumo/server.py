b2f00ef636d6e6206368e95245736491b52815de
Alfi Maulana <alfi.maulana.f@gmail.com>
2021-03-08 20:30:23 +0700

    Test fix

--- kumo/server.py
+++ kumo/server.py
@@ -1,3 +1,23 @@
+# Copyright (c) 2021 Ichiro ITS
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
 import asyncio
 import websockets
 
@@ -8,17 +28,17 @@ WebSocket = websockets.WebSocketServerProtocol
 
 class Server:
 
-  def __init__(self, host: str = 'localhost', port: int = 8080):
-    self.host = host
-    self.port = port
+    def __init__(self, host: str = 'localhost', port: int = 8080):
+        self.host = host
+        self.port = port
 
-  async def listen(self, websocket: WebSocket, path: str):
-    session = Session(websocket)
-    await session.listen()
+    async def listen(self, websocket: WebSocket, path: str):
+        session = Session(websocket)
+        await session.listen()
 
-  def run(self):
-    print('Starting server on %s with port %d...' % (self.host, self.port))
-    websocket = websockets.serve(self.listen, self.host, self.port)
+    def run(self):
+        print('Starting server on %s with port %d...' % (self.host, self.port))
+        websocket = websockets.serve(self.listen, self.host, self.port)
 
-    asyncio.get_event_loop().run_until_complete(websocket)
-    asyncio.get_event_loop().run_forever()
+        asyncio.get_event_loop().run_until_complete(websocket)
+        asyncio.get_event_loop().run_forever()
--- kumo/session.py
+++ kumo/session.py
@@ -1,9 +1,30 @@
+# Copyright (c) 2021 Ichiro ITS
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
 import asyncio
 import json
-from typing import Dict, List
+from typing import Dict
 import websockets
 
-from .handlers import CreateNodeHandler, CreateSubscriptionHandler, MessageHandler, NodeHandler, SubscriptionHandler
+from .handlers import (CreateNodeHandler, CreateSubscriptionHandler, MessageHandler,
+                       NodeHandler, SubscriptionHandler)
 
 WebSocket = websockets.WebSocketServerProtocol
 CreateSubHandler = CreateSubscriptionHandler
@@ -19,10 +40,9 @@ class Session:
         self.sub_handler = SubHandler(self.node_handler)
 
         self.message_handlers: Dict[str, MessageHandler] = {}
-        for message_handler in [
-            CreateNodeHandler(self.node_handler),
-            CreateSubHandler(self.sub_handler),
-            self.sub_handler.sub_callback_handler]:
+        for message_handler in [CreateNodeHandler(self.node_handler),
+                                CreateSubHandler(self.sub_handler),
+                                self.sub_handler.sub_callback_handler]:
 
             self.message_handlers[message_handler.type] = message_handler
 
